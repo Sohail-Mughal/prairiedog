99fc53da4d84a2cc47888361cadd33a156fbf28d
ottemw <ottemw@4af126ba-b44e-11de-aa42-85eb4131a9e0>
2010-09-14 17:25:17 +0000

    decided to work on this in an experimental branch

--- multi_robot_planner_cu/ad-hoc-config.sh
+++
@@ -1,7 +0,0 @@
-# remember to turn off networking first
-echo "Enter the desired ip of this computer: "
-read theip
-ifconfig wlan0 down
-ifconfig wlan0 up
-iwconfig wlan0 essid robots mode ad-hock
-ifconfig wlan0 $theip
